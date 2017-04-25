6c873270ea1afc776698ffc8f2654e0321b3b992
Marc Schubhan <s8mcschu@stud.uni-saarland.de>
2017-04-25 11:23:12 +0200

    setGameModel for thinh

--- source/main/gui/GuiAnchor.java
+++ source/main/gui/GuiAnchor.java
@@ -2,12 +2,16 @@ package gui;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import model.GameModel;
 
 /**
  * Created by Marc on 24.04.2017.
  */
 public class GuiAnchor extends Application {
 
+    public static Stage stage = null;
+    public static ProtoController cont = null;
+    public static GameModel gameModel = null;
 
     public static void main(String[] args) {
         launch(args);
@@ -16,5 +20,9 @@ public class GuiAnchor extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
+    }
+
+    public void setGameModel(GameModel gm) {
+        GuiAnchor.gameModel = gm;
     }
 }
