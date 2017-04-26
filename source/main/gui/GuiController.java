6be12e193ff8f350eab913654b352604c3ec088e
Marc Schubhan <s8mcschu@stud.uni-saarland.de>
2017-04-26 18:38:17 +0200

    start.fxml improved

--- source/main/gui/GuiController.java
+++ source/main/gui/GuiController.java
@@ -1,8 +1,13 @@
 package gui;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -18,11 +23,56 @@ import java.util.ResourceBundle;
 public class GuiController implements Initializable, IObserver {
 
     @FXML
-    ToggleButton fullscreenToggle = new ToggleButton();
+    ComboBox fullscreenComboBox = new ComboBox();
+    @FXML
+    ComboBox platformComboBox = new ComboBox();
+    @FXML
+    ComboBox gamemodeComboBox = new ComboBox();
+
+    @FXML
+    TextField channelNameField = new TextField();
+
+    private boolean fullscreen = true;
+    private String platform = "Twitch", channel = "k3uleee", gamemode = "Free for all";
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        fullscreenToggle.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("pictures/american-flag.png"))));
+
+        fullscreenComboBox.getItems().addAll("On", "Off");
+        platformComboBox.getItems().addAll("Twitch", "Beam");
+        gamemodeComboBox.getItems().addAll("Free for all", "Streamer Explains");
+
+        fullscreenComboBox.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                if(fullscreenComboBox.getValue().toString().equals("On"))
+                    fullscreen = true;
+                else
+                    fullscreen = false;
+            }
+        });
+
+        platformComboBox.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                platform = platformComboBox.getValue().toString();
+            }
+        });
+
+        channelNameField.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                channel = channelNameField.getText();
+            }
+        });
+
+        gamemodeComboBox.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                gamemode = gamemodeComboBox.getValue().toString();
+                System.out.println(gamemode);
+            }
+        });
     }
 
     @Override
