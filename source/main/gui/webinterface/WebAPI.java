b0677a6375465a6b8ccef6777da0c756115e04f5
Tim Düwel <s8tiduew@stud.uni-saarland.de>
2017-04-29 15:12:04 +0200

    springboot + thymeleaf integration

---
+++ source/main/gui/webinterface/WebAPI.java
@@ -0,0 +1,18 @@
+package gui.webinterface;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebAPI {
+
+    @RequestMapping("/")
+    public String settings() {
+        return "setup";
+    }
+
+    @RequestMapping("/register")
+    public String register() {
+        return "register";
+    }
+}
