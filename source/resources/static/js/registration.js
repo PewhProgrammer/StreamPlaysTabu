5320a9f5b776f761a2ae08ff64f0278a77fff87c
Marc Schubhan <s8mcschu@stud.uni-saarland.de>
2017-04-30 16:37:49 +0200

    further improvements to game.html

---
+++ source/resources/static/js/registration.js
@@ -0,0 +1,13 @@
+var timeLeft = 30;
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+    timeLeft = timeLeft - 1;
+
+    document.getElementById("countdown").innerHTML = timeLeft + "s";
+    document.getElementById("progressbar").style.width = (timeLeft / 30) * 100 + "%";
+
+    if (timeLeft == 0) {
+        timeLeft = 30;
+    }
+}, 1000);
